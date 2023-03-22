09018cc2c83290960d0c1044454cc32e113a51c1
cail0906 <97146971+cail0906@users.noreply.github.com>
2023-03-22 11:00:03 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,31 @@
+## 简介
+
+fofatools 是一个用python 编写的FOFA批量查询脚本，利用强大的互联网搜索引擎FoFa，能进行批量查询，让网络安全专业人士更容易在目标网站上寻找漏洞。
+
+## 使用说明
+
+本工具基于 FoFa 的 API 进行封装，使用时需要高级会员或者普通会员的 API key，使用注册用户的 API key 会提示账户需要充值F币。
+
+点击 https://github.com/cail0906/fofatools下载
+
+- pip install pandas==1.5.3,requests==2.28.2
+- 将查询语句写入fofa_queries.txt
+- python fofa.py
+- 查看fofa_results.csv
+
+
+
+**FOFA会员说明链接!!!!!**
+
+贵宾说明：https://fofa.info/static_pages/vip
+
+
+
+## 问答
+
+1. 为什么查询显示结果超过了10000条，但是导出的数据不到10000条？
+   由于fofa api的限制，每个查询条件最多获取10000条数据，所以超过10000条以后的数据将无法导出，可尝试通过关键字和限制查询时间来获取数据。`after``before`
+
+2. 为什么挂了梯子无法执行脚本？
+
+   由于外网无法直接访问api
---
+++ fofa.py
@@ -0,0 +1,53 @@
+import requests
+import csv
+import base64
+
+# FOFA API的基础URL
+BASE_URL = "https://fofa.info/api/v1/search/all"
+
+# FOFA email和key，需要替换成自己的
+FOFA_EMAIL = "please input your mail!"
+FOFA_KEY = "please input your apikey!"
+
+# 从txt文件中读取FOFA语句
+def read_queries(file_path):
+    queries = []
+    with open(file_path, "r") as f:
+        for line in f:
+            queries.append(line.strip())
+    return queries
+
+# 查询FOFA API并将结果存储在CSV文件中
+def query_fofa_api(query):
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
+    params = {
+        "email": FOFA_EMAIL,
+        "key": FOFA_KEY,
+        "qbase64": base64.b64encode(query.encode('utf-8')).decode('utf-8'),
+        "fields": "host,ip,title,port,protocol"
+    }
+    response = requests.get(BASE_URL, params=params, headers=headers, timeout=100)
+    if response.status_code == 200:
+        results = response.json()["results"]
+        write_to_csv(results)
+    else:
+        print("Failed to query FOFA API for query: {}".format(query))
+
+# 将结果写入CSV文件中
+def write_to_csv(results):
+    with open("fofa_results.csv", "a", newline="", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        for result in results:
+            result_str = str(result)
+            try:
+                host, ip, title, port, protocol = result_str.split(",")
+                writer.writerow([host, ip, title, port, protocol])
+            except ValueError as e:
+                print("Error processing result: {}".format(result))
+                print(e)
+
+# 主函数
+if __name__ == "__main__":
+    queries = read_queries("fofa_queries.txt")
+    for query in queries:
+        query_fofa_api(query)
